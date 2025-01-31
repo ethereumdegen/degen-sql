b9a7044d3e041ce519ece5314ae6c9c10a134481
andy <admazzola@gmail.com>
2025-01-31 16:01:01 -0500

    improve error

--- src/db/postgres/models/model.rs
+++ src/db/postgres/models/model.rs
@@ -1,10 +1,15 @@
+use std::mem::discriminant;
+
 use serde_json::Error as SerdeJsonError;
 use tokio_postgres::Error as PostgresError;
 
 pub trait Model {}
 
-#[derive(Debug, thiserror::Error)]
+#[derive(Debug, thiserror::Error )]
 pub enum PostgresModelError {
+    #[error("Timeout")]
+    Timeout,
+
     #[error(transparent)]
     Postgres(#[from] PostgresError),
 
@@ -15,3 +20,11 @@ pub enum PostgresModelError {
     RowParseError(Option<String>),
 
 }
+
+impl PartialEq for PostgresModelError {
+    fn eq(&self, other: &Self) -> bool {
+        discriminant(self) == discriminant(other)
+    }
+}
+
+impl Eq for PostgresModelError {}
